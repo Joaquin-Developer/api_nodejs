9e227796d244b580cda555477417124668724163
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-03-27 15:01:17 -0300

    auth logic

---
+++ api/components/auth/controller.js
@@ -0,0 +1,24 @@
+const table = ""
+
+module.exports = function(injectedStore) {
+    let store = injectedStore;
+    if (!store) {
+        store = require("../../store/dummy");
+    }
+
+    function upsert(data) {
+        const authData = {
+            id: data.id
+        }
+
+        if (data.username)
+            authData.username = data.username;
+
+        if (data.password)
+            authData.password = data.password;
+
+        return store.upsert(table, authData);
+    }
+
+    return { upsert };
+}
